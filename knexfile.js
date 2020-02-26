cf02bd653e6d6bf8935acf4ff6eba98b70780eef
andre-rondon <surplus@Andre-macbook-pro.local>
2020-02-26 11:54:28 -0800

    knex sql

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg', // use the pg library as our psql clint
+    connection: { //  configures how to connects to our database
+      database: 'wadd_knex' // tell knex to use this database
+    }
+  },
+
+};
